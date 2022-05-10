cde02eaf75dacd2a6d4713fe438d1e4e0e84d4fd
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2022-05-10 02:25:30 +0000

    [Add]いいね多い順

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,8 @@
 class Favorite < ApplicationRecord
 
-  belongs_to :user
   belongs_to :book
+  belongs_to :user
 
-  validates_uniqueness_of :book_id, scope: :user_id 
+
+  validates_uniqueness_of :book_id, scope: :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :book
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -29,9 +30,9 @@ class User < ApplicationRecord
       @user = User.where("name LIKE?", "%#{word}%")
     else
       @user = User.all
-    end 
-  end 
-  
+    end
+  end
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
